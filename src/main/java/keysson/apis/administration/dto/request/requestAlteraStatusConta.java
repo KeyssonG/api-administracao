25799d83d4173724948638f1afeb3a76e0dee0a3
keysson Gomes <104814645+KeyssonG@users.noreply.github.com>
2025-05-01 20:56:18 -0300

    Método de alteração de Status da Conta

---
+++ src/main/java/keysson/apis/administration/dto/request/requestAlteraStatusConta.java
@@ -0,0 +1,12 @@
+package keysson.apis.administration.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class requestAlteraStatusConta {
+
+    private int newStatus;
+
+}
