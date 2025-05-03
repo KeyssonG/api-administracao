572c3c5a9b2fa302ce7251a5e7d4f20986a09caa
keysson Gomes <104814645+KeyssonG@users.noreply.github.com>
2025-05-02 23:43:13 -0300

    Correção da importação da classe

--- src/main/java/keysson/apis/administration/controller/AdministrationController.java
+++ src/main/java/keysson/apis/administration/controller/AdministrationController.java
@@ -9,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+@RequestMapping("/administracao")
 public interface AdministrationController {
 
     @GetMapping("/empresa/pendente/")
--- src/main/java/keysson/apis/administration/controller/AdministrationControllerImpl.java
+++ src/main/java/keysson/apis/administration/controller/AdministrationControllerImpl.java
@@ -6,6 +6,7 @@ import keysson.apis.administration.exception.BusinessRuleException;
 import keysson.apis.administration.service.AdministrationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
