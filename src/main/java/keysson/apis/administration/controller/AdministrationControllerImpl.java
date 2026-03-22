3206075cfb8ff79d799190adb3a878143e295074
keysson Gomes <keyssoncardoso0@gmail.com>
2026-03-21 23:08:50 -0300

    Regras de validação de modulos

--- src/main/java/keysson/apis/administration/controller/AdministrationControllerImpl.java
+++ src/main/java/keysson/apis/administration/controller/AdministrationControllerImpl.java
@@ -10,6 +10,7 @@ import keysson.apis.administration.exception.BusinessRuleException;
 import keysson.apis.administration.service.AdministrationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
